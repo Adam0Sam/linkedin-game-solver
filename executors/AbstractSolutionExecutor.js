c4e83f6f348d7bef5048c54b7b434b3383372b07
Adam0Sam <el.naujienos@gmail.com>
2025-06-21 20:26:48 +0300

    Simulate real user click instead of .click() invocation

--- executors/AbstractSolutionExecutor.js
+++ executors/AbstractSolutionExecutor.js
@@ -25,6 +25,41 @@ export class AbstractSolutionExecutor extends AbstractClass {
   }
 
   /**
+   * Simulates a real click on the element by dispatching mousedown, mouseup, and click events.
+   * This is useful for ensuring that the click behaves like a user interaction.
+   * @param {HTMLElement} element
+   */
+  #simulateRealClick(element) {
+    const mouseDownEvent = new MouseEvent("mousedown", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      clientX: element.getBoundingClientRect().left + 5,
+      clientY: element.getBoundingClientRect().top + 5,
+      button: 0,
+    });
+
+    const mouseUpEvent = new MouseEvent("mouseup", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      clientX: element.getBoundingClientRect().left + 5,
+      clientY: element.getBoundingClientRect().top + 5,
+      button: 0,
+    });
+
+    const clickEvent = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+    });
+
+    element.dispatchEvent(mouseDownEvent);
+    element.dispatchEvent(mouseUpEvent);
+    element.dispatchEvent(clickEvent);
+  }
+
+  /**
    * @param {Document} doc
    * @param {GridCell[][]} solutionGrid
    * @throws {Error}
@@ -35,8 +70,8 @@ export class AbstractSolutionExecutor extends AbstractClass {
     for (const [rowIndex, colIndex] of clickSequence) {
       const cell = gridElements[rowIndex][colIndex];
       if (cell) {
-        console.log(`Clicking cell at (${rowIndex}, ${colIndex})`, cell);
-        cell.click();
+        this.#simulateRealClick(cell);
+        this.#simulateRealClick(cell);
       } else {
         throw new Error(`Cell at (${rowIndex}, ${colIndex}) does not exist.`);
       }
