1cc4c982aeb71c053f32404497c6b9d922b0818a
Adam0Sam <el.naujienos@gmail.com>
2025-07-19 19:44:45 +0300

    Auto script

---
+++ scripts/templates/{{cmpNamePascalCase}}GameSolver.js
@@ -0,0 +1,12 @@
+import { AbstractGameSolver } from '../common/abstract-helpers/AbstractGameSolver';
+
+export class {{cmpNamePascalCase}}GameSolver extends AbstractGameSolver {
+    constructor(grid) {
+        super(grid);
+    }
+
+    solve() {
+        // Barebones solve method
+        return [];
+    }
+}
---
+++ scripts/templates/{{cmpNamePascalCase}}GridParser.js
@@ -0,0 +1,13 @@
+import { AbstractGridParser } from '../common/abstract-helpers/AbstractGridParser';
+import { {{cmpNamePascalCase}}GridCell } from './{{cmpNamePascalCase}}GridCell';
+
+export class {{cmpNamePascalCase}}GridParser extends AbstractGridParser {
+    constructor() {
+        super();
+    }
+
+    parse(gridId) {
+        // Barebones parse method
+        return [];
+    }
+}
