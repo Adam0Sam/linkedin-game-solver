550583030f03f856207e995621092ec6a276a446
Adam0Sam <el.naujienos@gmail.com>
2025-08-03 17:03:30 +0300

    ZipGridSnapshot hasSolution method

--- games/zip/ZipGridSnapshot.js
+++ games/zip/ZipGridSnapshot.js
@@ -1,17 +1,33 @@
 import { AbstractGridSnapshot } from "../common/abstract-helpers/AbstractGridSnapshot.js";
 import { Path } from "./solver/Path.js";
+import { ZipGridCell } from "./ZipGridCell.js";
 
 export class ZipGridSnapshot extends AbstractGridSnapshot {
+  #traversedCellCount = 0;
+  /**
+   * @param {number} count
+   */
+  increaseTraversedCellCount(count) {
+    this.#traversedCellCount += count;
+  }
+
   /**
    * @param {Path} path
    */
   traversePath(path) {
+    /**
+     * @type {ZipGridSnapshot}
+     */
     const newSnapshot = this.clone();
-    for (const cell of path.getPathCells()) {
+    const pathCells = path.getPathCells();
+    newSnapshot.increaseTraversedCellCount(pathCells.length);
+    for (const cell of pathCells) {
       newSnapshot.setCellState(cell, "cross");
     }
     return newSnapshot;
   }
 
-  hasSolution() {}
+  hasSolution() {
+    return this.#traversedCellCount === this.gridSize * this.gridSize;
+  }
 }
